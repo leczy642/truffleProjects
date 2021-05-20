9b08b432b3ead523628b817e4ed182d0779834af
leczy <leczy642@yahoo.com>
2021-05-21 00:14:10 +0100

    Added Interface.sol
    An illustration of how to declare an interface in solidity

--- TestProject/contracts/Interface.sol
+++ TestProject/contracts/Interface.sol
@@ -3,19 +3,25 @@ pragma solidity >=0.6.0;
 
 interface IERC20 {
 
+    //declare the setTotalSupply interface function
      function setTotalSupply(uint256 amount) external;
 
+     //declare the totalSupply interface function to output the totalSupply
      function totalSupply() external view returns (uint256);
 }
 
 contract Token is IERC20{
 
-   uint256 _totalSupply;
+    uint256 _totalSupply;
+
+    //declaring this function must include the override keyword since we
+    //are extending an interface
     function setTotalSupply(uint256 amount_) external override{
         _totalSupply = amount_;
     }
+    //declaring this function must include the override keyword since we
+    //are extending an interface
     function totalSupply() external view override returns (uint256) {
     return _totalSupply;
   }
 }
-    
